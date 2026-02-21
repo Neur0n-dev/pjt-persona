dc01fa7e006a1e3679149ed1b2bde84cb5e27cba
neur0n <ghbaek88@gmail.com>
2026-02-21 13:53:10 +0900

    feat: 프로젝트 초기 세팅

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [
+    {
+      name: 'persona',
+      script: 'node_modules/.bin/next',
+      args: 'start',
+      cwd: '/var/www/persona',
+      instances: 1,
+      exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3020,
+      },
+      out_file: './logs/out.log',
+      error_file: './logs/error.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      restart_delay: 3000,
+      max_restarts: 10,
+    },
+  ],
+}
